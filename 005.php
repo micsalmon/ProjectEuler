159244b30a26bc45329ff2f84e9979f93eab0703
Michael Salmon <michael.salmon@lampcamp.net>
2015-07-14 21:32:11 +0000

    005 getting closer

--- 005.php
+++ 005.php
@@ -4,15 +4,19 @@
  * SMALLEST MULTIPLE
  *
  * 2520 is the smallest number that can be divided by each of the numbers
- * from 1 to 10 without any remainder. 
+ * from 1 to 10 without any remainder.
  *
  * What is the smallest positive number that is evenly divisible by all
  * of the numbers from 1 to 20 ?
  */
 
-$num = 1;	
+$final = 0;
+$num = 0;
 for ($x = 1; $x <= 20; $x++) {
-	while ($num % $x == 0) {
+	if ($num % $x) {
+		$final = $num;
+		printf($final . "\n");
+	} else {
 		$num++;
 	}
 }
